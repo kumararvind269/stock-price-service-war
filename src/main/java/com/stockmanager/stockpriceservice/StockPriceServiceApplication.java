cb0a70c1ef3beb15288e589a8b45776328b0bfde
kumararvind269 <kumararvind269@gmail.com>
2018-03-21 12:37:39 +0530

    f

--- src/main/java/com/stockmanager/stockpriceservice/StockPriceServiceApplication.java
+++ src/main/java/com/stockmanager/stockpriceservice/StockPriceServiceApplication.java
@@ -2,11 +2,20 @@ package com.stockmanager.stockpriceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StockPriceServiceApplication {
+public class StockPriceServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockPriceServiceApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(StockPriceServiceApplication.class);
+	}
+	
 }
